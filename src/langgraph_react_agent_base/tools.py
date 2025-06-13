0c88981226128c0221bd5f6c313179be95e933f8
liawyihui <yi.hui.liaw@ibm.com>
2025-06-13 15:24:53 +0800

    update prompt

--- src/langgraph_react_agent_base/tools.py
+++ src/langgraph_react_agent_base/tools.py
@@ -4,12 +4,15 @@ from langchain_core.tools import tool
 @tool("search", parse_docstring=True)
 def dummy_web_search(query: str) -> list[str]:
     """
-    Web search tool that return static list of strings.
+    You are a helpful supplier researcher that uses tools to answer questions
+precisely after doing thorough research on google and document search. The google
+search tool can provide customer sentiment on the suppliers. The document search tool
+has the procurement requisition rules and past year supplier review.
 
     Args:
-        query: User query to search in web.
+        query: User query to search in documents and web.
 
     Returns:
-        Dummy list of web search results.
+        Dummy list of search results.
     """
     return ["IBM watsonx.ai"]
